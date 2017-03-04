81fdd12c6dbb59385db0b5abeabaa4ff2dcfd9f6
Raphael Bialon <bialon@cs.uni-duesseldorf.de>
2017-03-04 12:45:42 +0100

    Fix shift detection

--- pulsor.py
+++ pulsor.py
@@ -9,31 +9,48 @@ def get_subface_coord(fh_x, fh_y, fh_w, fh_h, face_rect):
                 	int(w * fh_w),
                 	int(h * fh_h)]
 
+def shift(detected):
+  x, y, w, h = detected
+  center = np.array([x + 0.5 * w, y + 0.5 * h])
+  newshift = np.linalg.norm(center - shift.last_center)
+
+  shift.last_center = center
+  return newshift
+shift.last_center = np.array([0,0])
 
 def main():
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 	cam = cv2.VideoCapture(-1)
+        face_rect = [1, 1, 2, 2]
 	while(True):
 		ret, frame = cam.read()
 		grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		img = frame
+		faces = list(face_cascade.detectMultiScale(grayscale, 1.3, 5))
+                
 
-		faces = face_cascade.detectMultiScale(grayscale, 1.3, 5)
-		# Draw all faces
-		for (x,y,w,h) in faces:
-			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-			
-			# Draw forehead region
-    			fx, fy, fw, fh = get_subface_coord(0.5, 0.18, 0.25, 0.15, (x, y, w, h))
-    			cv2.rectangle(img, (fx, fy), (fx+fw, fy+fh), (0,0,255), 2)
+                # Only update rect if face has shifted
+                if len(faces) > 0:
+                    # Sort faces
+                    faces.sort(key=lambda a: a[-1] * a[-2])
 
-			roi_gray = grayscale[y:y+h, x:x+w]
-    			roi_color = img[y:y+h, x:x+w]
-    			eyes = eye_cascade.detectMultiScale(roi_gray)
-    			# Draw all eyes
-    			for (ex,ey,ew,eh) in eyes:
-    				cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                    if shift(faces[-1]) > 10:
+                        face_rect = faces[-1]
+
+		(x,y,w,h) = face_rect
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                
+                # Draw forehead region
+                fx, fy, fw, fh = get_subface_coord(0.5, 0.18, 0.25, 0.15, (x, y, w, h))
+                cv2.rectangle(img, (fx, fy), (fx+fw, fy+fh), (0,0,255), 2)
+
+                roi_gray = grayscale[y:y+h, x:x+w]
+                roi_color = img[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                # Draw all eyes
+                for (ex,ey,ew,eh) in eyes:
+                        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 
 		cv2.imshow("camera", img)
