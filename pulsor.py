41d6e455d2eee516e9c889e7cb88ec62b5402165
Alexander Schneider <alexander@schneider.gg>
2017-03-04 12:27:34 +0100

    add forehead detection

--- pulsor.py
+++ pulsor.py
@@ -2,6 +2,14 @@ import cv2
 import numpy as np
 
 
+def get_subface_coord(fh_x, fh_y, fh_w, fh_h, face_rect):
+	x, y, w, h = face_rect
+	return [int(x + w * fh_x - (w * fh_w / 2.0)),
+                	int(y + h * fh_y - (h * fh_h / 2.0)),
+                	int(w * fh_w),
+                	int(h * fh_h)]
+
+
 def main():
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -12,13 +20,21 @@ def main():
 		img = frame
 
 		faces = face_cascade.detectMultiScale(grayscale, 1.3, 5)
+		# Draw all faces
 		for (x,y,w,h) in faces:
 			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+			
+			# Draw forehead region
+    			fx, fy, fw, fh = get_subface_coord(0.5, 0.18, 0.25, 0.15, (x, y, w, h))
+    			cv2.rectangle(img, (fx, fy), (fx+fw, fy+fh), (0,0,255), 2)
+
 			roi_gray = grayscale[y:y+h, x:x+w]
     			roi_color = img[y:y+h, x:x+w]
     			eyes = eye_cascade.detectMultiScale(roi_gray)
+    			# Draw all eyes
     			for (ex,ey,ew,eh) in eyes:
     				cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
 
 		cv2.imshow("camera", img)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
